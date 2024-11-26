5a6eaeb44d7550cb3519d44ae2134a2148fa786b
Sabarish <avsabarish@gmail.com>
2024-11-26 18:52:08 +0530

    feat[FE]: api integration account record transactions page

--- client/src/components/Transaction/index.jsx
+++ client/src/components/Transaction/index.jsx
@@ -1,16 +1,24 @@
+/* eslint-disable react/prop-types */
 import style from "./style.module.css";
 
-const Transaction = ()=>{
+const Transaction = ({bgColor, transaction})=>{
+    const dateString=transaction.createdAt;
+    const dateObj = new Date(dateString);
+    const date = dateObj.toLocaleDateString("en-CA");
+    // date is in yyyy-mm-dd format
+    //below code is to convert it to dd-mm-yyyy
+    const [year, month, day] = date.split('-');
+    const formattedDate = `${day}-${month}-${year}`;
     return (
-        <div className={style.transactionCard}>
+        <div style={{backgroundColor:bgColor}} className={style.transactionCard}>
             <div className={style.transactionDateContainer}>
-                <p>01-02-2024</p>
+                <p>{formattedDate}</p>
             </div>
             <div className={style.transactionType}>
-                <p>DEBIT</p>
+                <p>{transaction.transactionType}</p>
             </div>
             <div className={style.transactionAmountContainer}>
-                <p><span>500</span>Rs</p>
+                <p><span>{transaction.amount}</span>Rs</p>
             </div>
         </div>
     )
--- client/src/pages/TransactionsForAccountRecord/index.jsx
+++ client/src/pages/TransactionsForAccountRecord/index.jsx
@@ -1,24 +1,75 @@
+import { useEffect, useState } from "react";
 import Transaction from "../../components/Transaction";
-import style from './style.module.css'
+import getUserId from "../../middleware/getUserId";
+import style from "./style.module.css";
+import Cookie from "js-cookie";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Pagination from "@mui/material/Pagination";
 
-const TransactionsForAccountRecord = ()=>{
-    return (
-        <div>
+const TransactionsForAccountRecord = () => {
+  const token = Cookie.get("token");
+  const userId = getUserId();
+  const { accountRecordId } = useParams();
+  const [pageData, setPageData] = useState();
+  const [accountRecordName, setAccountRecordName] = useState();
+  const [totalPages, setTotalPages] = useState();
+  const [pageNo, setPageNo] = useState(0);
+  const navigate = useNavigate();
+  const fetchPageData = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BE_URL}/transactions/${accountRecordId}?userId=${userId}&page=${pageNo}&size=5`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response) {
+        setPageData(response.data);
+        setAccountRecordName(response.data.accountRecordName);
+        setTotalPages(response.data.transactions.totalPages);
+      }
+    } catch {
+      navigate("/login");
+    }
+  };
 
-            <div className={style.transactionsHeading}>
-                <p>All transactions of <span>Sabarish</span></p>
-            </div>
+  useEffect(() => {
+    fetchPageData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageNo]);
 
-            <div className={style.transactionsContainer}>
-            <Transaction/>
-            <Transaction/>
-            <Transaction/>
-            <Transaction/>
-            <Transaction/>
-        </div>
+  return (
+    <div>
+      <div className={style.transactionsHeading}>
+        <p>
+          Transactions of <span>{accountRecordName}</span>
+        </p>
+      </div>
 
-        </div>
-    );
-}
+      <div className={style.transactionsContainer}>
+        {pageData?.transactions?.content?.map((transaction) => (
+          <Transaction
+            key={transaction.id}
+            bgColor={
+              transaction.transactionType == "CREDIT" ? "#02a145" : "#f10238"
+            }
+            transaction={transaction}
+          />
+        ))}
+      </div>
+      <Pagination
+        count={totalPages}
+        page={pageNo + 1}
+        color="primary"
+        onChange={(e, value) => {
+          setPageNo(value - 1);
+        }}
+      />
+    </div>
+  );
+};
 
-export default TransactionsForAccountRecord
+export default TransactionsForAccountRecord;
