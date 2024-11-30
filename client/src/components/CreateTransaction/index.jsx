4974de26c706e22bd04f253f6bfb7e13157ac5a6
Sabarish <avsabarish@gmail.com>
2024-11-30 11:08:14 +0530

    feat[FE]: api integration for create transaction

--- client/src/components/CreateTransaction/index.jsx
+++ client/src/components/CreateTransaction/index.jsx
@@ -3,24 +3,77 @@ import style from "./style.module.css";
 import { Input } from "@mui/base/Input";
 import Button from "@mui/material/Button";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import DoneIcon from "@mui/icons-material/Done";
 import { useState } from "react";
+import getUserId from "../../middleware/getUserId";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Cookie from "js-cookie";
 
 // eslint-disable-next-line react/prop-types
 const CreateTransaction = ({ display, setDisplay }) => {
   const [transactionAmountError, setTransactionAmountError] = useState();
-  // eslint-disable-next-line no-unused-vars
   const [transactionTypeError, setTransactionTypeError] = useState();
   const [transactionApiResponse, setTransactionApiResponse] = useState();
   const [amount, setAmount] = useState();
-  // eslint-disable-next-line no-unused-vars
   const [transactionType, setTransactionType] = useState();
-  // use .inputError class
+
+  const userId = getUserId();
+  const { accountRecordId } = useParams();
+  const token = Cookie.get("token");
 
   const createTransaction = async () => {
-    if (parseFloat(amount) <= 0) {
-      setTransactionAmountError(
+    try {
+      if (!amount) {
+        setTransactionAmountError(
+          <p className={style.inputError}>
+            <ErrorOutlineIcon /> This field is mandatory
+          </p>
+        );
+        return;
+      }
+      if (!transactionType) {
+        setTransactionTypeError(
+          <p className={style.inputError}>
+            <ErrorOutlineIcon /> This field is mandatory
+          </p>
+        );
+        return;
+      }
+      if (parseFloat(amount) <= 0) {
+        setTransactionAmountError(
+          <p className={style.inputError}>
+            <ErrorOutlineIcon /> Amount must be greater than 0
+          </p>
+        );
+        return;
+      }
+      await axios.post(
+        `${import.meta.env.VITE_BE_URL}/transactions/`,
+        {
+          userId,
+          accountRecordId,
+          amount,
+          transactionType,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setTransactionApiResponse(
+        <p style={{ color: "green" }} className={style.inputError}>
+          <DoneIcon /> Transaction created successfully!!
+        </p>
+      );
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch {
+      setTransactionApiResponse(
         <p className={style.inputError}>
-          <ErrorOutlineIcon /> Amount must be greater than 0
+          <ErrorOutlineIcon /> Something went wrong!!
         </p>
       );
     }
@@ -52,6 +105,7 @@ const CreateTransaction = ({ display, setDisplay }) => {
               onChange={(e) => {
                 setAmount(e.target.value);
                 setTransactionApiResponse();
+                setTransactionAmountError();
               }}
             />
             {transactionAmountError}
@@ -62,6 +116,8 @@ const CreateTransaction = ({ display, setDisplay }) => {
               required
               onChange={(e) => {
                 setTransactionType(e.target.value);
+                setTransactionApiResponse();
+                setTransactionTypeError();
               }}
             >
               <option value="">Select an option</option>
