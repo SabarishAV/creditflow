1570cf20743048c67954be9a83fca395398a28ea
Sabarish <avsabarish@gmail.com>
2024-12-06 17:27:29 +0530

    feat[BE]: change dockerfile to multi stage and remove dotev

--- server/src/main/java/com/creditflow/creditflow/CreditflowApplication.java
+++ server/src/main/java/com/creditflow/creditflow/CreditflowApplication.java
@@ -3,17 +3,20 @@ package com.creditflow.creditflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CreditflowApplication {
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
+		// Dotenv dotenv = Dotenv.load();
 
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", System.getenv("DB_URL"));
+		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+		// System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		// System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		// System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(CreditflowApplication.class, args);
     }
