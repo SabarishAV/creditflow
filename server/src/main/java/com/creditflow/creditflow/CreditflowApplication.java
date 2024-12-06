d03f2ba5d9b19ba68efd80247dbe6819f8e6aa31
Sabarish <avsabarish@gmail.com>
2024-12-06 15:41:03 +0530

    feat[BE]: implemented environment variables

--- server/src/main/java/com/creditflow/creditflow/CreditflowApplication.java
+++ server/src/main/java/com/creditflow/creditflow/CreditflowApplication.java
@@ -3,11 +3,19 @@ package com.creditflow.creditflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CreditflowApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(CreditflowApplication.class, args);
-	}
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(CreditflowApplication.class, args);
+    }
 
 }
